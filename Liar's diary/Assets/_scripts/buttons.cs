2da1e375c09dac3d2c96f7cc4a93bfaabcdab582
mi5kas <el.miskauskas@gmail.com>
2019-05-28 22:35:09 +0300

    base

---
+++ Liar's diary/Assets/_scripts/buttons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class buttons : MonoBehaviour
+{
+    [SerializeField] string function;
+    [SerializeField] GameObject[] forFunction;
+    TextMeshPro component;
+    // Start is called before the first frame update
+    void Start()
+    {
+        component = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    private void OnMouseEnter()
+    {
+        component.color = new Color32(255, 255, 255, 255);
+    }
+    private void OnMouseExit()
+    {
+        component.color = new Color32(161, 161, 97, 255);
+    }
+    private void OnMouseDown()
+    {
+        Invoke(function, 0f);
+    }
+    void OpenSettings()
+    {
+        forFunction[0].GetComponent<Animator>().CrossFadeInFixedTime("flipAnim", 2f);
+        forFunction[1].GetComponent<Animator>().CrossFadeInFixedTime("flipAnim", 2f);
+        forFunction[2].SetActive(false);
+        forFunction[3].SetActive(true);
+    }
+    void BackToMenu()
+    {
+        forFunction[0].GetComponent<Animator>().CrossFadeInFixedTime("Empty", 2f);
+        forFunction[1].GetComponent<Animator>().CrossFadeInFixedTime("Empty", 2f);
+    }
+}
