df63d89acbc26feab5391e9d0e1a3f2905a619e0
mi5kas <33864467+mi5kas@users.noreply.github.com>
2020-11-10 16:56:04 +0200

    first

---
+++ Liar's diary/Assets/fps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fps : MonoBehaviour
+{
+    float deltaTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        this.GetComponent<Text>().text = Mathf.Ceil(fps).ToString();
+    }
+}
